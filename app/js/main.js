20b197cb96c0cb5eb51fdc0847248ca481fc513c
AloneWolf <max_sk@mail.ru>
2015-01-09 15:16:53 +0200

    edit feature

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
-define(['jquery', 'socket.io', 'canvas/canvas.stage', 'canvas/canvas.view', 'contextmenu', 'object/draw.object'],
-	function($, socket, stage, canvas, menu, draw){
+define(['jquery', 'socket.io', 'canvas/canvas.stage', 'canvas/canvas.view', 'contextmenu', 'object/object.draw', 'object/object.edit'],
+	function($, socket, stage, canvas, menu, draw, edit){
 		stage.init();
 		menu.init({$stage: stage.$stage});
 		draw.init({$stage: stage.$stage});
+		edit.init({$stage: stage.$stage});
 	});
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var app = require('./server')(),
     io = require('socket.io')(app.http),
     socket = require('./socket')(io);
 
-socket.initialize();
 app.initialize();
+socket.initialize();
+
 
