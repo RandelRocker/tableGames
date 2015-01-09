5f281a20f27dc4adbfed3e54cd98ade32d14f661
AloneWolf <max_sk@mail.ru>
2015-01-09 23:42:35 +0200

    grunt build test

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-define(['jquery', 'socket.io', 'canvas/canvas.stage', 'canvas/canvas.view', 'contextmenu', 'object/object.draw', 'object/object.edit'],
-	function($, socket, stage, canvas, menu, draw, edit){
-		stage.init();
-		menu.init({$stage: stage.$stage});
-		draw.init({$stage: stage.$stage});
-		edit.init({$stage: stage.$stage});
-	});
--- main.js
+++
@@ -1,8 +0,0 @@
-var app = require('./server')(),
-    io = require('socket.io')(app.http),
-    socket = require('./socket')(io);
-
-app.initialize();
-socket.initialize();
-
-
